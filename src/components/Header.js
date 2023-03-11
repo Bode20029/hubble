9799df8b5c9c2be8b88e13d9db08731e4a7346cc
Bode <jry.bode@gmail.com>
2023-03-11 18:30:04 +0700

    b

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+import { StyledHeader} from './styles/Header.styled'
 export default function Header(){
     return (
-      <header>
+      <StyledHeader>
         <h1>Hubble</h1>
-        </header>  
+        </StyledHeader>  
     )
 }
